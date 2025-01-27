8ebdfc8deca046bc239bd151d2483acacb0bc5ac
Yash Chauhan <yashc9411407582@gmail.com>
2025-01-27 05:36:30 +0530

    Format code for consistency and readability in tuples example

--- 02-Tupes in Python/Tuples.py
+++ 02-Tupes in Python/Tuples.py
@@ -1,24 +1,24 @@
-#Tuples :ordered , immutable , allows duplicate elements
+# Tuples :ordered , immutable , allows duplicate elements
 
 
-mytuple=(3,4,"Max",True) #tuple
-
-print(mytuple)
-
-print(type(mytuple)) # class tuple
-
-
-#single element would be considered as string
-mytuple=("apple")
+mytuple = (3, 4, "Max", True)  # tuple
 
 print(mytuple)
 
-print(type(mytuple)) #class str
+print(type(mytuple))  # class tuple
 
 
-#to make it tuple
+# single element would be considered as string
+mytuple = "apple"
 
-mytuple=tuple(["apple","banana","mango","cherry"])
+print(mytuple)
+
+print(type(mytuple))  # class str
+
+
+# to make it tuple
+
+mytuple = tuple(["apple", "banana", "mango", "cherry"])
 print(mytuple)
 print(type(mytuple))
 
@@ -26,19 +26,18 @@ print(type(mytuple))
 ## or use the , after the value to make it sure that it is tuple
 
 
-
-mytuple2=("banana",)#adding comma after entry
+mytuple2 = ("banana",)  # adding comma after entry
 
 print(mytuple2)
 
 print(type(mytuple2))
 
 
-#getting elements
-item=mytuple[2]
+# getting elements
+item = mytuple[2]
 print(item)
 
-#It also supports the negative indexing
+# It also supports the negative indexing
 
 
 # mytuple[0]="Tim" throws error since tuple is immutable
@@ -48,78 +47,81 @@ for x in mytuple:
     print(x)
 
 
-#checking elements exist in it or not
+# checking elements exist in it or not
 if "cherry" in mytuple:
-    print("yes") 
+    print("yes")
 else:
     print("no")
 
-#to get the length of the tuple
+# to get the length of the tuple
 
 print(len(mytuple))
 
-print(mytuple.count('cherry')) #return the number of occurences in tuple for example here the output is 1 since it exists only one time in the tuple 
+print(
+    mytuple.count("cherry")
+)  # return the number of occurences in tuple for example here the output is 1 since it exists only one time in the tuple
 
 
-print("first occurence of cherry in the tuple is at index ",mytuple.index("cherry"))#return the first occurence index of the element 
+print(
+    "first occurence of cherry in the tuple is at index ", mytuple.index("cherry")
+)  # return the first occurence index of the element
 
 
-#converting the tuple to the list
- 
+# converting the tuple to the list
 
-mylist=list(mytuple)
+
+mylist = list(mytuple)
 
 print(mytuple)
 print(mylist)
 
 
-mytuple=tuple(mylist)
+mytuple = tuple(mylist)
 print(mytuple)
 print(mylist)
 
 
-#slicing with tuples
+# slicing with tuples
 
-a=(2,3,4,23,53,25,64,52,51,54,32,52,1,0)
+a = (2, 3, 4, 23, 53, 25, 64, 52, 51, 54, 32, 52, 1, 0)
 
-b=a[2:5] #output (4, 23, 53) since last index is excluded like 5th index 64 is excluded
+b = a[
+    2:5
+]  # output (4, 23, 53) since last index is excluded like 5th index 64 is excluded
 print(b)
 
-c=a[::2] # this 2 is the step 
+c = a[::2]  # this 2 is the step
 print(c)
 
 
-#to reverse the tuple
+# to reverse the tuple
 
 
-c=a[::-1]
+c = a[::-1]
 print(c)
 
 
-#Unpacking 
+# Unpacking
 
-mytuple="Max",28,"Boston"
+mytuple = "Max", 28, "Boston"
 
-name,age,city=mytuple
+name, age, city = mytuple
 
 print(name)
 print(age)
 print(city)
 
-#no of variables on lhs == rhs otherwise the Value error may come like: too many values to unpack
+# no of variables on lhs == rhs otherwise the Value error may come like: too many values to unpack
 
 
-#if some situations occur 
+# if some situations occur
 
-mytuple=(4,2,5,1,0,9,3,2)
+mytuple = (4, 2, 5, 1, 0, 9, 3, 2)
 
-i1,*i2,i3=mytuple
+i1, *i2, i3 = mytuple
 print(i1)
-print(i2) #returns list after unpacking
+print(i2)  # returns list after unpacking
 print(i3)
 
 
-#Comparing the tuple and list
-
-
-
+# Comparing the tuple and list
