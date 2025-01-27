6cfa74718a6096d4655126fb279426771454ec97
Yash Chauhan <yashc9411407582@gmail.com>
2025-01-27 05:35:46 +0530

    Refactor timeit print statements for improved readability

--- 02-Tupes in Python/testing2.py
+++ 02-Tupes in Python/testing2.py
@@ -1,3 +1,8 @@
 import timeit
-print(timeit.timeit(stmt="[0,1,2,3,4,5]",number=1000000)) #takes more time to create the list /  0.17931300000054762
-print(timeit.timeit(stmt="(0,1,2,3,4,5)",number=1000000)) # takes less time to create the tuple / 0.06579429999692366
+
+print(
+    timeit.timeit(stmt="[0,1,2,3,4,5]", number=1000000)
+)  # takes more time to create the list /  0.17931300000054762
+print(
+    timeit.timeit(stmt="(0,1,2,3,4,5)", number=1000000)
+)  # takes less time to create the tuple / 0.06579429999692366
